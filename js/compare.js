ae9dcea752405057ab90f37b49ee0081d091cbb7
MATEUS SILVA DOS ANJOS <017.632787@senaicimatec.edu.br>
2025-08-20 16:07:17 -0300

    Comparação de veículos

--- js/compare.js
+++ js/compare.js
@@ -3,10 +3,18 @@
 let carArr = [];
 
 class Car {
-   
-
     constructor(nome, preco, alturaCacamba, alturaVeiculo, alturaSolo, capacidadeCarga, motor, potencia, volumeCacamba, roda, image){
-       
+        this.nome = nome;
+        this.preco = preco;
+        this.alturaCacamba = alturaCacamba;
+        this.alturaVeiculo = alturaVeiculo;
+        this.alturaSolo = alturaSolo;
+        this.capacidadeCarga = capacidadeCarga;
+        this.motor = motor;
+        this.potencia = potencia;
+        this.volumeCacamba = volumeCacamba;
+        this.roda = roda;
+        this.image = image;
     }
 } 
 
@@ -23,7 +31,11 @@ function SetCarToCompare(el, carClass) {
    
     if(carClass instanceof Car){       
         if(el.checked){
-                
+            if(carArr.length >= 2){
+                el.checked = false;
+                alert("Você só pode comparar 2 veículos por vez!")
+                return;
+            }      
             
         } else {
           
